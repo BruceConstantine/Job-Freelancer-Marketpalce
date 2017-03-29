ab310fe28823b7cd42678eb53fd5f244eb7ed004
Lenovo <16006245@studentmail.ul.ie>
2017-03-29 14:16:19 +0100

    jason

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication1;
+
 
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
